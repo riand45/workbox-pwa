9747c1976eb0839ac65101322d5410e7c053ac77
Riand Pratama <riandpratama4570@gmail.com>
2021-04-15 08:03:18 +0700

    include template and register service worker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Arrived from "./components/Arrived";
+import AsideMenu from "./components/AsideMenu";
+import Browse from "./components/Browse";
+import Client from "./components/Client";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrived />
+      <Client />
+      <AsideMenu />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer class="flex text-center px-4 py-8 justify-center">
+      <p class="text-sm">
+        Copyright 2021 • All Rights Reserved PWA React JS by Unknown
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
